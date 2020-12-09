c89f4f9e28156e51fb5574582eff82bbc5bbc85f
acoreynews <acorey07@gmail.com>
2020-12-08 21:44:12 -0800

    tweaks to static

--- cookbook/settings.py
+++ cookbook/settings.py
@@ -120,6 +120,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+
+
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
