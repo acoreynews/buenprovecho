60beca37ea751bbacf271fd65490e93a8d07d5fb
acoreynews <acorey07@gmail.com>
2020-12-08 20:46:15 -0800

    Making a tweak to settings.py

--- cookbook/settings.py
+++ cookbook/settings.py
@@ -70,8 +70,11 @@ ROOT_URLCONF = 'cookbook.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [
+        os.path.join(BASE_DIR, 'templates')
+        ],
         #'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
